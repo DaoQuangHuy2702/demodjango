6f52fd5e1a3632c1e2efa28c9c51e519668fb050
quanghuy <daoquanghuy2702@gmail.com>
2022-03-26 00:59:32 +0700

    update

--- demoweb/phone/views.py
+++ demoweb/phone/views.py
@@ -5,6 +5,8 @@ from .models import phoneModel
 from django.core.paginator import Paginator
 
 # Create your views here.
+
+
 def index(request):
     bm = phoneModel.objects.all()
     paginator = Paginator(bm, 4)
@@ -12,30 +14,33 @@ def index(request):
     page_obj = paginator.get_page(page_number)
     return render(request, 'phone/index.html', {'page_obj': page_obj})
 
+
 def phone_detail(request, id):
-    pd = phoneModel.objects.get(id = id)
+    pd = phoneModel.objects.get(id=id)
     return render(request, 'phone/detail.html', {'pd': pd})
+
 
 def add_phone(request):
     pf = phoneForm(request.POST, request.FILES)
     if pf.is_valid():
         pf.save()
         return redirect('phone:phone')
-    
+
     return render(request, 'phone/add.html', {'pf': pf})
 
+
 def phone_delete(request, id):
-    pd = phoneModel.objects.get(id = id)
+    pd = phoneModel.objects.get(id=id)
     pd.delete()
 
     return redirect('phone:phone')
 
+
 def phone_update(request, id):
-    pd = phoneModel.objects.get(id = id)
-    pf = phoneForm(request.POST or None, request.FILES or None, instance=ld)
+    pd = phoneModel.objects.get(id=id)
+    pf = phoneForm(request.POST or None, request.FILES or None, instance=pd)
     if pf.is_valid():
         pf.save()
         return redirect('phone:phone')
-    
-    return render(request, 'phone/update.html', {'pf': pf})
 
+    return render(request, 'phone/update.html', {'pf': pf})
