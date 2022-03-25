b537ccb79c55d1d48d5daa3ec33dad54df3f2510
quanghuy <daoquanghuy2702@gmail.com>
2022-03-25 23:45:13 +0700

    update

--- demoweb/phone/urls.py
+++ demoweb/phone/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'phone'
 urlpatterns = [
-    path('', views.index, name = "phone")
+    path('', views.index, name = "phone"),
+    path('<int:id>/', views.phone_detail, name = "detail"),
+    path('delete/<int:id>/', views.phone_delete, name = "delete"),
+    path('add/', views.add_phone, name="add"),
+    path('update/<int:id>/', views.phone_update, name = "update"),
 ]
--- demoweb/phone/views.py
+++ demoweb/phone/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import phoneForm
+from .models import phoneModel
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    return render(request, 'phone/index.html');
+    bm = phoneModel.objects.all()
+    paginator = Paginator(bm, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'phone/index.html', {'page_obj': page_obj})
+
+def phone_detail(request, id):
+    pd = phoneModel.objects.get(id = id)
+    return render(request, 'phone/detail.html', {'pd': pd})
+
+def add_phone(request):
+    pf = phoneForm(request.POST, request.FILES)
+    if pf.is_valid():
+        pf.save()
+        return redirect('phone:phone')
+    
+    return render(request, 'phone/add.html', {'pf': pf})
+
+def phone_delete(request, id):
+    pd = phoneModel.objects.get(id = id)
+    pd.delete()
+
+    return redirect('phone:phone')
+
+def phone_update(request, id):
+    pd = phoneModel.objects.get(id = id)
+    pf = phoneForm(request.POST or None, request.FILES or None, instance=ld)
+    if pf.is_valid():
+        pf.save()
+        return redirect('phone:phone')
+    
+    return render(request, 'phone/update.html', {'pf': pf})
+
